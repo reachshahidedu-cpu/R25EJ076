e3a8b8e2d21c0d91d736446c6f92c3d4da16701f
reachshahidedu-cpu <reachshahid.edu@gmail.com>
2026-06-10 22:16:37 +0530

    Added canvas initialization and display function

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
+#define ROWS 20
+#define COLS 40
+
+char canvas[ROWS][COLS];
+
+void clearCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    for(int i=0;i<ROWS;i++)
+    {
+        for(int j=0;j<COLS;j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf("2D Graphics Editor\n");
+    clearCanvas();
+    displayCanvas();
     return 0;
 }
